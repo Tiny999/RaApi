db997e61f17abb2ac86e6112aee1136e2601cc9f
Abbas Salami <olawalesalami3@gmail.com>
2022-07-01 18:17:01 +0100

    ra api

---
+++ src/api/bath/controllers/bath.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  bath controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::bath.bath');
---
+++ src/api/bath/routes/bath.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * bath router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::bath.bath');
